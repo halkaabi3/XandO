350632cd1683718caa6d91547481d40fc2753a36
halkaabi3 <halkaabi3@mail.csuchico.edu>
2018-04-05 23:02:36 -0700

    X/O UPDATE

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ const char BLANK = ' ';
 const char X = 'X';
 const char O = 'O';
 const int QUIT = -1;
-
+char winner =' ';
 int main()
 {
         //3x3 matrix of characters, initially with blank spaces in each position
@@ -82,6 +82,40 @@ int main()
                         }
                         cout<<endl;
                 }
+
+if(' ' != board[0] [0] && board[0] [0] == board[0] [1] && board[0] [0] == board[0] [2]||' ' != board[0][0] && board[0] [0] == board[1] [0] && board[0] [0] == board[2] [0] || ' ' != board[0] [0] && board[0] [0] == board[2] [2])
+
+{
+winner = board[0] [0];
+playing = false;
+cout<<"Winner is "<<winner<<endl;
+}
+
+else if(' ' != board[2] [2] && board[2] [2] == board[1] [2] && board[2] [2] == board[0] [2] || ' ' != board[2] [2] && board[2] [2] == board[2] [0] && board[2] [2] == board[2] [1])
+
+
+{
+winner = board[2] [2];
+playing = false;
+cout<<"Winner is "<<winner<<endl;
+}
+
+else if(' ' != board[1] [1] && board[1] [1] == board[0] [1] && board[1] [1] == board[2] [1]||' ' != board[1][1] && board[1] [1] == board[1] [0] && board[1] [1] == board[1] [2] || ' ' != board[1] [1] && board[1] [1] == board[2] [0] && board[1] [1] == board[0] [2])
+
+{
+winner = board[1] [1];
+playing = false;
+cout<<"Winner is "<<winner<<endl;
+}
+
+if(row == -1 && column == -1)
+{
+playing = false;
+}
+
+
+
+
 
         }while( playing );
 
